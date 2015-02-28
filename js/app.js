6a6f05bda6ebbc6b7dfdd78c8dc7b82bd7382d10
Jeff Dean <jeff@galvanize.it>
2015-02-28 13:23:41 -0700

    added concept of turns

--- js/app.js
+++ js/app.js
@@ -2,44 +2,52 @@
 
 // 0 = blank, 1 = white, 2 = black
 var Othello = function(){
-  this._board = {};
-  this._init();
+  this.turn = 2;
 };
 
-Othello.prototype._init = function () {
-  var columns = 'abcdefgh'.split('');
-  for (var i = 1; i < 9; i++) {
-    this._board[i] = {};
-    columns.forEach(function (columnName) {
-      this._board[i][columnName] = 0;
-    }.bind(this));
-  }
-  this.place(1, 'd', 4);
-  this.place(1, 'e', 5);
-  this.place(2, 'd', 5);
-  this.place(2, 'e', 4);
+Othello.columnNames = {a:0, b:1, c:2, d:3, e:4, f:5, g:6, h:7};
+
+Othello.prototype._translate = function (columnName, rowName) {
+  return {x: Othello.columnNames[columnName], y: rowName - 1};
 };
 
 Othello.prototype.board = function () {
-  var result = [],
-      row,
-      columnName,
-      rowName;
+  this._ensureBoard();
+  return this._board;
+};
 
-  for(rowName in this._board) {
-    row = [];
-    for (columnName in this._board[rowName]) {
-      row.push(this._board[rowName][columnName]);
+Othello.prototype._ensureBoard = function () {
+  if(!this._board) {
+    this._board = [];
+    for (var i = 0; i < 8; i++) {
+      this._board[i] = [];
+      for (var j = 0; j < 8; j++) {
+        this._board[i][j] = 0;
+      }
     }
-    result.push(row);
+    this.place(2, 'd', 5);
+    this.place(1, 'e', 5);
+    this.place(2, 'e', 4);
+    this.place(1, 'd', 4);
   }
-  return result;
 };
 
 Othello.prototype.place = function (value, columnName, rowName) {
+  this._ensureBoard();
+  var point = this._translate(columnName, rowName);
   var opposite = value === 1 ? 2 : 1;
-  this._board[rowName][columnName] = value;
-  if(this._board[rowName + 1][columnName] === opposite && this._board[rowName + 2][columnName] === value) {
-    this._board[rowName + 1][columnName] = value;
+  this._board[point.y][point.x] = value;
+  if(this._board[point.y + 1][point.x] === opposite && this._board[point.y + 2][point.x] === value) {
+    this._board[point.y + 1][point.x] = value;
   }
+  if(this._board[point.y][point.x + 1] === opposite && this._board[point.y][point.x + 2] === value) {
+    this._board[point.y][point.x + 1] = value;
+  }
+  if(this._board[point.y - 1][point.x] === opposite && this._board[point.y - 2][point.x] === value) {
+    this._board[point.y - 1][point.x] = value;
+  }
+  if(this._board[point.y][point.x - 1] === opposite && this._board[point.y][point.x - 2] === value) {
+    this._board[point.y][point.x - 1] = value;
+  }
+  this.turn = opposite;
 };
--- spec/spec/AppSpec.js
+++ spec/spec/AppSpec.js
@@ -12,9 +12,10 @@ describe("Othello Board", function() {
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
     ]);
+    expect(othello.turn).toEqual(2); // black
   });
 
-  it("allows valid placements of black in the first move", function() {
+  it("allows initial placement of black in d3", function() {
     var othello = new Othello();
     othello.place(2, 'd', 3);
     expect(othello.board()).toEqual([
@@ -23,6 +24,52 @@ describe("Othello Board", function() {
       [0, 0, 0, 2, 0, 0, 0, 0],
       [0, 0, 0, 2, 2, 0, 0, 0],
       [0, 0, 0, 2, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+    expect(othello.turn).toEqual(1); // white
+  });
+
+  it("allows initial placement of black in c4", function() {
+    var othello = new Othello();
+    othello.place(2, 'c', 4);
+    expect(othello.board()).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 2, 2, 2, 0, 0, 0],
+      [0, 0, 0, 2, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it("allows initial placement of black in e6", function() {
+    var othello = new Othello();
+    othello.place(2, 'e', 6);
+    expect(othello.board()).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 2, 0, 0, 0],
+      [0, 0, 0, 2, 2, 0, 0, 0],
+      [0, 0, 0, 0, 2, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it("allows initial placement of black in f5", function() {
+    var othello = new Othello();
+    othello.place(2, 'f', 5);
+    expect(othello.board()).toEqual([
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 2, 0, 0, 0],
+      [0, 0, 0, 2, 2, 2, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
