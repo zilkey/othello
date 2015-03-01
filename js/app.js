7d3c9cd820f3b0f6c307c5481b22029544c02171
Jeff Dean <jeff@galvanize.it>
2015-02-28 21:12:06 -0700

    can flip multiple pieces north and south

--- js/app.js
+++ js/app.js
@@ -38,32 +38,94 @@ Othello.prototype.place = function (value, columnName, rowName) {
   var point = this._translate(columnName, rowName);
   var opposite = value === 1 ? 2 : 1;
   this._board[point.y][point.x] = value;
-  if(this._board[point.y + 1][point.x] === opposite && this._board[point.y + 2][point.x] === value) {
-    this._board[point.y + 1][point.x] = value;
+
+  // south
+  if(this._board[point.y + 1] && this._board[point.y + 1][point.x] === opposite) {
+    var canFlipSouth = false;
+    var piecesToFlip = [];
+    var index = 2;
+    var keepGoing = true;
+    piecesToFlip.push({y: point.y + 1, x: point.x});
+
+    while (!canFlipSouth && keepGoing) {
+      if (this._board[point.y + index] && this._board[point.y + index][point.x] === value) {
+        canFlipSouth = true;
+      } else if (this._board[point.y + index] && this._board[point.y + index][point.x] === opposite) {
+        piecesToFlip.push({y: point.y + index, x: point.x});
+      } else {
+        keepGoing = false;
+      }
+      index++;
+    }
+
+    if (canFlipSouth) {
+      for (var i = 0; i < piecesToFlip.length; i++) {
+        var newPoint = piecesToFlip[i];
+        this._board[newPoint.y][newPoint.x] = value;
+      }
+    }
   }
+
+  // east
   if(this._board[point.y][point.x + 1] === opposite && this._board[point.y][point.x + 2] === value) {
     this._board[point.y][point.x + 1] = value;
   }
-  if(this._board[point.y - 1][point.x] === opposite && this._board[point.y - 2][point.x] === value) {
-    this._board[point.y - 1][point.x] = value;
+
+  // north
+  if(this._board[point.y - 1] && this._board[point.y - 1][point.x] === opposite) {
+    var canFlipNorth = false;
+    var piecesToFlip = [];
+    var index = 2;
+    var keepGoing = true;
+    piecesToFlip.push({y: point.y - 1, x: point.x});
+
+    while (!canFlipNorth && keepGoing) {
+      if (this._board[point.y - index][point.x] === value) {
+        canFlipNorth = true;
+      } else if (this._board[point.y - index][point.x] === opposite) {
+        piecesToFlip.push({y: point.y - index, x: point.x});
+      } else {
+        keepGoing = false;
+      }
+      index++;
+    }
+
+    if (canFlipNorth) {
+      for (var i = 0; i < piecesToFlip.length; i++) {
+        var newPoint = piecesToFlip[i];
+        this._board[newPoint.y][newPoint.x] = value;
+      }
+    }
   }
+
+  // west
   if(this._board[point.y][point.x - 1] === opposite && this._board[point.y][point.x - 2] === value) {
     this._board[point.y][point.x - 1] = value;
   }
-  if(this._board[point.y + 1][point.x + 1] === opposite && this._board[point.y + 2][point.x + 2] === value) {
-    this._board[point.y + 1][point.x + 1] = value;
+
+  // south east
+  if(this._board[point.y + 1]) {
+    if(this._board[point.y + 1][point.x + 1] === opposite && this._board[point.y + 2][point.x + 2] === value) {
+      this._board[point.y + 1][point.x + 1] = value;
+    }
   }
-  if(this._board[point.y - 1][point.x - 1] === opposite && this._board[point.y - 2][point.x - 2] === value) {
+
+  // north west
+  if(this._board[point.y - 1] && this._board[point.y - 1][point.x - 1] === opposite && this._board[point.y - 2][point.x - 2] === value) {
     this._board[point.y - 1][point.x - 1] = value;
   }
-  if(this._board[point.y + 1][point.x - 1] === opposite && this._board[point.y + 2][point.x - 2] === value) {
-    this._board[point.y + 1][point.x - 1] = value;
+
+  // south west
+  if(this._board[point.y + 1]) {
+    if(this._board[point.y + 1][point.x - 1] === opposite && this._board[point.y + 2][point.x - 2] === value) {
+      this._board[point.y + 1][point.x - 1] = value;
+    }
   }
-  if(this._board[point.y + 1][point.x - 1] === opposite && this._board[point.y + 2][point.x - 2] === value) {
-    this._board[point.y + 1][point.x - 1] = value;
-  }
-  if(this._board[point.y - 1][point.x + 1] === opposite && this._board[point.y - 2][point.x + 2] === value) {
+
+  // north east
+  if(this._board[point.y - 1] && this._board[point.y - 1][point.x + 1] === opposite && this._board[point.y - 2][point.x + 2] === value) {
     this._board[point.y - 1][point.x + 1] = value;
   }
+
   this.turn = opposite;
 };
